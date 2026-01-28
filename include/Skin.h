6cb55b59f2e6f60c08acafd7bd74896eac7e3c73
aslanchan7 <124756060+aslanchan7@users.noreply.github.com>
2026-01-28 02:55:22 -0800

    skinning works

--- include/Skin.h
+++ include/Skin.h
@@ -1,35 +1,30 @@
 #pragma once
 
 #include <vector>
-#include "core.h"
-#include "Vertex.h"
-#include "Triangle.h"
+#include "Tokenizer.h"
+#include <iostream>
+#include "Skeleton.h"
 #include <tuple>
 
 class Skin {
-	public:
-		Skin();
-		~Skin();
+public:
+    void Update();
+    bool Load(const char* file);
+    void Draw(const glm::mat4& viewProjMtx, GLuint shader);
 
-		bool Load(const char* file);
-		void Update();
-		void Draw(const glm::mat4& viewProjMtx, GLuint shader);
+private:
+    std::vector<glm::vec3> positions; 
+    std::vector<glm::vec3> normals;
 
-	private:
-		std::vector<Vertex*> vertices;
-		std::vector<Triangle*> triangles;
-		std::vector<std::vector<int>> vertexIndices;
-		std::vector<glm::mat4> bindings;
-		
-		std::vector<glm::mat4> skinMats;
-		std::vector<glm::mat4> skinMatsInvT;
-		
-		std::vector<glm::vec3> newVertices;
-		std::vector<glm::vec3> newNormals;
-		
-		int jointCount;
-		int vertexCount;
-		int triangleCount;
-		
-		bool initialized;
+    std::vector<std::vector<std::tuple<int, float>>> skinWeights; 
+    std::vector<glm::ivec3> triangleIndices; 
+
+    std::vector<glm::mat4> bindings;
+
+    int vertexCount;
+    int triangleCount;
+    int jointCount;
+
+    std::vector<glm::vec3> newPositions;
+    std::vector<glm::vec3> newNormals; 
 };
--- src/Skin.cpp
+++ src/Skin.cpp
@@ -1,179 +1,162 @@
 #include "Skin.h"
-#include "Skeleton.h"
-#include <iostream>
-
-Skin::Skin() {
-	vertices = std::vector<Vertex*>{};
-	triangles = std::vector<Triangle*>{};
-	bindings = std::vector<glm::mat4>{};
-	jointCount = 0;
-	vertexCount = 0;
-	triangleCount = 0;
-	initialized = false;
-}
-
-Skin::~Skin() {
-	for each(Vertex* vertex in vertices)
-	{
-		delete vertex;
-	}
-	
-	for each(Triangle * triangle in triangles)
-	{
-		delete triangle;
-	}
-}
-
-bool Skin::Load(const char* file) {
-	Tokenizer tokenizer;
-	bool success = tokenizer.Open(file);
-	
-	tokenizer.FindToken("positions");
-	int n = tokenizer.GetInt();
-	vertexCount = n;
-	tokenizer.FindToken("{");
-	for (int i = 0; i < n; i++)
-	{
-		Vertex* vertex = new Vertex();
-		vertex->position = glm::vec3( tokenizer.GetFloat(), tokenizer.GetFloat(), tokenizer.GetFloat() );
-		vertices.push_back(vertex);
-	}
-	tokenizer.FindToken("}");
-
-	tokenizer.FindToken("normals");
-	tokenizer.FindToken("{");
-	for (int i = 0; i < n; i++)
-	{
-		vertices[i]->normal = glm::vec3( tokenizer.GetFloat(), tokenizer.GetFloat(), tokenizer.GetFloat() );
-	}
-	tokenizer.FindToken("}");
-
-	tokenizer.FindToken("skinweights");
-	tokenizer.FindToken("{");
-	for (int i = 0; i < n; i++)
-	{
-		int m = tokenizer.GetInt();
-		for (int j = 0; j < m; j++)
-		{
-			int jointIndex = tokenizer.GetInt();
-			float weight = tokenizer.GetFloat();
-			vertices[i]->skinWeight.push_back(std::tuple<int, float>(jointIndex, weight));
-		}
-	}
-	tokenizer.FindToken("}");
-
-	tokenizer.FindToken("triangles");
-	triangleCount = tokenizer.GetInt();
-	tokenizer.FindToken("{");
-	for (int i = 0; i < triangleCount; i++)
-	{
-		int idx1 = tokenizer.GetInt();
-		int idx2 = tokenizer.GetInt();
-		int idx3 = tokenizer.GetInt();
-
-		//std::vector<int> vertexIndices = {};
-		std::vector<int> triangleIndices = { idx1, idx2, idx3 };
-		vertexIndices.push_back(triangleIndices);
-	}
-	tokenizer.FindToken("}");
-
-	tokenizer.FindToken("bindings");
-	jointCount = tokenizer.GetInt();
-	tokenizer.FindToken("{");
-	for (int i = 0; i < jointCount; i++)
-	{
-		tokenizer.FindToken("{");
-		float ax = tokenizer.GetFloat();
-		float ay = tokenizer.GetFloat();
-		float az = tokenizer.GetFloat();
-
-		float bx = tokenizer.GetFloat();
-		float by = tokenizer.GetFloat();
-		float bz = tokenizer.GetFloat();
-
-		float cx = tokenizer.GetFloat();
-		float cy = tokenizer.GetFloat();
-		float cz = tokenizer.GetFloat();
-
-		float dx = tokenizer.GetFloat();
-		float dy = tokenizer.GetFloat();
-		float dz = tokenizer.GetFloat();
-
-		glm::mat4 binding = {
-			ax, ay, az, 0.0f,
-			bx, by, bz, 0.0f,
-			cx, cy, cz, 0.0f,
-			dx, dy, dz, 1.0f
-		};
-		bindings.push_back(binding);
-	}
-	tokenizer.FindToken("}");
-
-	tokenizer.Close();
-	return success;
-}
 
 void Skin::Update() {
-	skinMats = std::vector<glm::mat4>(jointCount);
-	skinMatsInvT = std::vector<glm::mat4>(jointCount);
-	newVertices = std::vector<glm::vec3>(vertexCount);
-	newNormals = std::vector<glm::vec3>(vertexCount);
 
-	// foreach joint
-	for (int i = 0; i < jointCount; i++)
-	{
-		glm::mat4 jointMat = Skeleton::GetWorldMatrix(i);
-		glm::mat4 skinMat = jointMat * glm::inverse(bindings[i]);
-		glm::mat4 skinMatInvTran = glm::inverse(glm::transpose(skinMat));
+    std::vector<glm::mat4> skinMats(jointCount);
+	std::vector<glm::mat4> skinMatInvT(jointCount);
+    for (int i = 0; i < jointCount; i++) {
+        glm::mat4 worldMat = Skeleton::GetWorldMatrix(i);
+        glm::mat4 inverseBindingMat = glm::inverse(bindings[i]);
+        skinMats[i] = worldMat * inverseBindingMat;
+        glm::mat4 skinMatInvTran = glm::inverse(glm::transpose(skinMats[i]));
+		skinMatInvT[i] = skinMatInvTran;
+    }
 
-		skinMats[i] = skinMat;
-		skinMatsInvT[i] = skinMatInvTran;
-	}
+    for (int i = 0; i < vertexCount; i++) {
+        glm::vec3 newPos(0.0f);
+        glm::vec3 newNorm(0.0f);
 
-	for (int i = 0; i < vertexCount; i++)
-	{
-		glm::vec3 newPos(0.0f);
-		glm::vec3 newNorm(0.0f);
+        for (auto weight : skinWeights[i]) {
+            glm::vec4 pos = skinMats[std::get<0>(weight)] * glm::vec4(positions[i], 1.0f);
+            newPos += std::get<1>(weight) * glm::vec3(pos);
 
-		for (auto weight : vertices[i]->skinWeight) {
-			glm::mat4 M = skinMats[std::get<0>(weight)];
-			glm::vec4 pos = M * glm::vec4(vertices[i]->position, 1.0f);
-			newPos += std::get<1>(weight) * glm::vec3(pos);
+            glm::vec4 norm = skinMatInvT[std::get<0>(weight)] * glm::vec4(normals[i], 0.0f);
+            newNorm += std::get<1>(weight) * glm::vec3(norm);
 
-			glm::vec4 norm = M * glm::vec4(vertices[i]->normal, 0.0f);
-			newNorm += std::get<1>(weight) * glm::vec3(norm);
-		}
-
-		newVertices[i] = newPos;
-		newNormals[i] = glm::normalize(newNorm);
-	}
-
-	if (!initialized) {
-		triangles = std::vector<Triangle*>(triangleCount);
-		for (int i = 0; i < triangleCount; i++)
-		{
-			std::vector<glm::vec3> positions = {
-				newVertices[vertexIndices[i][0]],
-				newVertices[vertexIndices[i][1]],
-				newVertices[vertexIndices[i][2]]
-			};
-
-			std::vector<glm::vec3> normals = {
-				newNormals[vertexIndices[i][0]],
-				newNormals[vertexIndices[i][1]],
-				newNormals[vertexIndices[i][2]]
-			};
-
-			Triangle* triangle = new Triangle(positions, normals);
-			triangles[i] = triangle;
-		}
-		initialized = true;
-	}
+        }
+        newPositions[i] = newPos;
+        newNormals[i] = glm::normalize(newNorm);
+    }
 }
 
 void Skin::Draw(const glm::mat4& viewProjMtx, GLuint shader) {
-	for (int i = 0; i < triangles.size(); i++)
-	{
-		triangles[i]->Draw(viewProjMtx, shader);
-	}
+    // Draw triangles using transformed positions & normals
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadMatrixf(&viewProjMtx[0][0]);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    // Uses GL_TRIANGLE instead of triangle class
+    glBegin(GL_TRIANGLES);
+
+    for (int i = 0; i < triangleCount; i++) {
+        int v0 = triangleIndices[i].x;
+        int v1 = triangleIndices[i].y;
+        int v2 = triangleIndices[i].z;
+        //Vertex 0
+        glNormal3fv(&newNormals[v0][0]);
+        glVertex3fv(&newPositions[v0][0]);
+
+        //Vertex 1 
+        glNormal3fv(&newNormals[v1][0]);
+        glVertex3fv(&newPositions[v1][0]);
+
+        //Vertex 2
+        glNormal3fv(&newNormals[v2][0]);
+        glVertex3fv(&newPositions[v2][0]);
+    }
+
+    glEnd();
+}
+
+bool Skin::Load(const char* file) {
+    Tokenizer tokenizer;
+    tokenizer.Open(file);
+
+    tokenizer.FindToken("positions");
+    vertexCount = tokenizer.GetInt();
+    tokenizer.FindToken("{");
+	positions.resize(vertexCount);
+    for (int i = 0; i < vertexCount; i++)
+    {
+        positions[i].x = tokenizer.GetFloat();
+        positions[i].y = tokenizer.GetFloat();
+        positions[i].z = tokenizer.GetFloat();
+    }
+    tokenizer.FindToken("}");
+
+    tokenizer.FindToken("normals");
+    vertexCount = tokenizer.GetInt();
+    tokenizer.FindToken("{");
+    normals.resize(vertexCount);
+    for (int i = 0; i < vertexCount; i++)
+    {
+        normals[i] = glm::vec3(tokenizer.GetFloat(), tokenizer.GetFloat(), tokenizer.GetFloat());
+    }
+    tokenizer.FindToken("}");
+
+    tokenizer.FindToken("skinweights");
+    vertexCount = tokenizer.GetInt();
+    tokenizer.FindToken("{");
+    skinWeights.resize(vertexCount);
+    for (int i = 0; i < vertexCount; i++)
+    {
+        int m = tokenizer.GetInt();
+        skinWeights[i].resize(m);
+        for (int j = 0; j < m; j++)
+        {
+            int jointIndex = tokenizer.GetInt();
+            float weight = tokenizer.GetFloat();
+            std::tuple<int, float> skinWeight = std::tuple<int, float>(jointIndex, weight);
+            skinWeights[i][j] = skinWeight;
+        }
+    }
+    tokenizer.FindToken("}");
+
+    tokenizer.FindToken("triangles");
+    triangleCount = tokenizer.GetInt();
+    tokenizer.FindToken("{");
+	triangleIndices.resize(triangleCount);
+    for (int i = 0; i < triangleCount; i++)
+    {
+        int idx1 = tokenizer.GetInt();
+        int idx2 = tokenizer.GetInt();
+        int idx3 = tokenizer.GetInt();
+
+		glm::ivec3 triangleIndex = glm::ivec3(idx1, idx2, idx3);
+        triangleIndices[i] = triangleIndex;
+    }
+    tokenizer.FindToken("}");
+
+    tokenizer.FindToken("bindings");
+    jointCount = tokenizer.GetInt();
+    tokenizer.FindToken("{");
+	bindings.resize(jointCount);
+    for (int i = 0; i < jointCount; i++)
+    {
+        tokenizer.FindToken("{");
+        float ax = tokenizer.GetFloat();
+        float ay = tokenizer.GetFloat();
+        float az = tokenizer.GetFloat();
+
+        float bx = tokenizer.GetFloat();
+        float by = tokenizer.GetFloat();
+        float bz = tokenizer.GetFloat();
+
+        float cx = tokenizer.GetFloat();
+        float cy = tokenizer.GetFloat();
+        float cz = tokenizer.GetFloat();
+
+        float dx = tokenizer.GetFloat();
+        float dy = tokenizer.GetFloat();
+        float dz = tokenizer.GetFloat();
+
+        glm::mat4 binding = {
+            ax, ay, az, 0.0f,
+            bx, by, bz, 0.0f,
+            cx, cy, cz, 0.0f,
+            dx, dy, dz, 1.0f
+        };
+        bindings[i] = binding;
+		tokenizer.FindToken("}");
+    }
+    tokenizer.FindToken("}");
+
+    //deformed arrays
+    newPositions.resize(vertexCount);
+    newNormals.resize(vertexCount);
+
+    tokenizer.Close();
+    return true;
 }
